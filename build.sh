799a0bf994dcfa0376d039795e2f6829bb637224
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-04-25 22:38:55 +1000

    build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+for f in $(git diff --name-only HEAD^1)
+do
+  cd ${HOME}/build/${TRAVIS_REPO_SLUG}
+  pwd
+  echo $f
+  EXT=$(echo $f| rev | cut -f 1 -d'.' | rev )
+  echo "EXT => ${EXT}"
+  if test "${EXT}" = "cpp"
+  then
+    DIRNAME=$(dirname $f)
+    echo "DIRNAME => ${DIRNAME}"
+    cd ${DIRNAME}
+    BASENAME=$(basename $f .cpp)
+    echo "BASENAME => ${BASENAME}"
+    g++ -g -Wall ${BASENAME}.cpp -o ${BASENAME}
+    ./${BASENAME} < ${BASENAME}.in.txt > ${BASENAME}.result.txt
+    echo "Result is"
+    cat ${BASENAME}.result.txt
+    diff -Naur ${BASENAME}.result.txt ${BASENAME}.out.txt
+  fi
+done
