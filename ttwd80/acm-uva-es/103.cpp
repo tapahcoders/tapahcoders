e518fed100d96574847e9c39ea4cccd8c46cd959
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-04-25 19:48:42 +1000

    accepted

--- ttwd80/acm-uva-es/103.cpp
+++ ttwd80/acm-uva-es/103.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <map>
-#include <utility>
+#include <stack>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 typedef vector<int> vi;
@@ -16,16 +17,17 @@ bool fits_into(int s, int b, int n, int **source) {
 }
 
 int process_row(int value, int n, int **source, map<int, vi *> &count_map,
-                int max_count) {
+                int max_count, int *sequence) {
   for (int i = max_count; i >= 1; i--) {
     vi *vip = count_map[i];
     for (vi::iterator it = vip->begin(); it != vip->end(); ++it) {
-      cout << "testing with all elements with length = " << i << endl;
+      //cout << "testing with all elements with length = " << i << endl;
       if (fits_into(*it, value, n, source)) {
-        cout << *it << " is smaller than " << value << endl;
+        sequence[value] = *it;
+        //cout << *it << " is smaller than " << value << endl;
         return i + 1;
       } else {
-        cout << *it << " is NOT smaller than " << value << endl;
+        //cout << *it << " is NOT smaller than " << value << endl;
       }
     }
   }
@@ -46,34 +48,54 @@ void display_count_map(map<int, vi *> &count_map, int max_count) {
   }
 }
 
-void process(int k, int n, int **source) {
-  int *sequence = new int[k];
+void display_array(int *sequence, int k) {
+  cout << "[";
+  string sep = "";
+  for (int i = 0; i < k; i++) {
+    cout << sep << sequence[i];
+    sep = ", ";
+  }
+  cout << "]" << endl;
+}
+
+int process(int k, int n, int **source, int *sequence, int &track) {
   map<int, vi *> count_map;
   for (int i = 0; i < k; i++) {
-    sequence[i] = -1;
     count_map[i + 1] = new vi();
   }
 
   int max_count = 0;
   for (int i = 0; i < k; i++) {
-    int count = process_row(i, n, source, count_map, max_count);
+    int count = process_row(i, n, source, count_map, max_count, sequence);
     count_map[count]->push_back(i);
     max_count = max(max_count, count);
   }
 
-  display_count_map(count_map, max_count);
+  //display_count_map(count_map, max_count);
+  //display_array(sequence, k);
+  track = count_map[max_count]->at(0);
 
   for (int i = 0; i < k; i++) {
     delete count_map[i + 1];
   }
-  delete[] sequence;
+  return max_count;
 }
 
 bool compare(int *a, int *b) { return a[0] < b[0]; }
 void process(int k, int n) {
   int **source = new int *[k];
+  map<int, int *> position_box_map;
+  map<int *, int> box_position_map;
+  int *sequence = new int[k];
+
+  for (int i = 0; i < k; i++) {
+    sequence[i] = -1;
+  }
+
   for (int i = 0; i < k; i++) {
     source[i] = new int[n];
+    box_position_map[source[i]] = i;
+
   }
   for (int r = 0; r < k; r++) {
     for (int c = 0; c < n; c++) {
@@ -83,20 +105,44 @@ void process(int k, int n) {
   }
   sort(source, source + k, compare);
 
-  // display content
-  for (int r = 0; r < k; r++) {
-    for (int i = 0; i < n; i++) {
-      cout << source[r][i] << " ";
-    }
-    cout << endl;
+  for (int i = 0; i < k; i++) {
+    position_box_map[i] = source[i];
   }
 
-  process(k, n, source);
+  // display content
+  //for (int r = 0; r < k; r++) {
+    //for (int i = 0; i < n; i++) {
+      //cout << source[r][i] << " ";
+    //}
+    //cout << endl;
+  //}
+
+  int track = -1;
+  int max_count = process(k, n, source, sequence, track);
+  stack<int> stack;
+  while (track != -1) {
+    //cout << "track = " << track << endl;
+    int pos = box_position_map[position_box_map[track]];
+    //cout << "pos " << pos << endl;
+    stack.push(pos + 1);
+    //cout << track << ": ";
+    //display_array(source[track], n);
+    track = sequence[track];
+  }
+  cout << max_count << endl;
+  string sep = "";
+  while(!stack.empty()) {
+    cout << sep << stack.top();
+    stack.pop();
+    sep = " ";
+  }
+  cout << endl;
 
   for (int i = 0; i < k; i++) {
     delete[] source[i];
   }
   delete[] source;
+  delete[] sequence;
 }
 int main(int argc, char const *argv[]) {
   int k;
