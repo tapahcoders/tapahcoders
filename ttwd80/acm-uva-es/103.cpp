c47f99df6964616ef7c5c228bebdf0802db5b3a1
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-04-25 10:12:07 +1000

    we have the items, not with origin ranking and not in sequence, map[3] would list the biggest box that can contain 3 items.

--- ttwd80/acm-uva-es/103.cpp
+++ ttwd80/acm-uva-es/103.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 using namespace std;
 
+typedef vector<int> vi;
+
 bool fits_into(int s, int b, int n, int **source) {
   for (int i = 0; i < n; i++) {
     if (source[s][i] >= source[b][i]) {
@@ -13,7 +15,59 @@ bool fits_into(int s, int b, int n, int **source) {
   return true;
 }
 
+void print_row(int r, int n, int **source) {
+
+  string sep = "";
+  cout << "[";
+  for (int i = 0; i < n; i++) {
+    cout << sep << source[r][i];
+    sep = ", ";
+  }
+  cout << "]";
+}
+
+void append_to(map<int, vi*> &count_map, int count, int i) {
+  cout << "adding " << i << " to " << count << endl;
+  if (0 == count_map[count]) {
+    vi *vip = new vi();
+    vip->push_back(i);
+    count_map[count] = vip;
+  } else {
+    count_map[count]->push_back(i);
+    cout << "length of map[" << count << "] is now " << count_map[count]->size() << endl;
+  }
+}
+int process_row(int i, map<int, vi*> &count_map, int max_count, int k, int n, int **source) {
+  cout << "max count => " << max_count << endl;
+  for (int c = max_count; c >= 1; c--) {
+    vi *vip = count_map[c];
+    for (vi::iterator it = vip->begin(); vip->end() != it; ++it) {
+      if (fits_into(*it, i, n, source)) {
+        cout << (*it) << " fits into " << i << endl;
+        append_to(count_map, c + 1, i);
+        return c + 1;
+      } else {
+        cout << (*it) << " does not fit into " << i << endl;
+      }
+    }
+  }
+  return 0;
+}
 void process(int k, int n, int **source) {
+  map<int, vi*> count_map;
+  int max_count = 0;
+  for (int i = 0; i < k; i++) {
+    int count = process_row(i, count_map, max_count, k, n, source);
+    if (count > 0) {
+      max_count = max(max_count, count);
+    } else {
+      max_count = max(max_count, 1);
+      append_to(count_map, 1, i);
+    }
+    cout << endl;
+  }
+  cout << "max count = " << max_count << endl;
+  cout << count_map[max_count]->size() << endl;
 }
 
 bool compare(int *a, int *b) { return a[0] < b[0]; }
@@ -30,7 +84,7 @@ void process(int k, int n) {
   }
   sort(source, source + k, compare);
 
-  //display content
+  // display content
   for (int r = 0; r < k; r++) {
     for (int i = 0; i < n; i++) {
       cout << source[r][i] << " ";
