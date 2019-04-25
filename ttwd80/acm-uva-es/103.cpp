8c963a4ddd76f8fc52886dd5e1acea5f2aba02c0
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-04-25 17:45:36 +1000

    max sequence okay, now to print the sequene

--- ttwd80/acm-uva-es/103.cpp
+++ ttwd80/acm-uva-es/103.cpp
@@ -26,43 +26,49 @@ void print_row(int r, int n, int **source) {
   cout << "]";
 }
 
-void append_to(map<int, vi*> &count_map, int count, int i) {
-  //cout << "adding " << i << " to " << count << endl;
+void append_to(map<int, vi *> &count_map, int count, int i) {
+  // cout << "adding " << i << " to " << count << endl;
   if (0 == count_map[count]) {
     vi *vip = new vi();
     vip->push_back(i);
     count_map[count] = vip;
   } else {
     count_map[count]->push_back(i);
-    //cout << "length of map[" << count << "] is now " << count_map[count]->size() << endl;
+    // cout << "length of map[" << count << "] is now " <<
+    // count_map[count]->size() << endl;
   }
 }
-int process_row(int i, map<int, vi*> &count_map, int max_count, int k, int n, int **source) {
-  //cout << "max count => " << max_count << endl;
+int process_row(int i, map<int, vi *> &count_map, int max_count, int k, int n,
+                int **source) {
+  // cout << "max count => " << max_count << endl;
   for (int c = max_count; c >= 1; c--) {
     vi *vip = count_map[c];
     for (vi::iterator it = vip->begin(); vip->end() != it; ++it) {
       if (fits_into(*it, i, n, source)) {
-        //cout << (*it) << " fits into " << i << endl;
+        // cout << (*it) << " fits into " << i << endl;
         return c + 1;
       } else {
-        //cout << (*it) << " does not fit into " << i << endl;
+        // cout << (*it) << " does not fit into " << i << endl;
       }
     }
   }
   return 1;
 }
+void display_sequence() { cout << "xxx" << endl; }
+void display_max_count(int max_count) { cout << max_count << endl; }
 void process(int k, int n, int **source) {
-  map<int, vi*> count_map;
+  map<int, vi *> count_map;
   int max_count = 0;
   for (int i = 0; i < k; i++) {
     int count = process_row(i, count_map, max_count, k, n, source);
     append_to(count_map, count, i);
     max_count = max(max_count, count);
-    //cout << endl;
+    // cout << endl;
   }
-  //cout << "max count = " << max_count << endl;
-  //cout << count_map[max_count]->size() << endl;
+  display_max_count(max_count);
+  display_sequence();
+  // cout << "max count = " << max_count << endl;
+  // cout << count_map[max_count]->size() << endl;
 }
 
 void display_content(int k, int n, int **source) {
@@ -89,8 +95,7 @@ void process(int k, int n) {
   sort(source, source + k, compare);
 
   // display content
-  //display_content(k, n, source);
-
+  // display_content(k, n, source);
 
   process(k, n, source);
 
